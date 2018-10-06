20ce18cd98a5fad798f4e4a4c0a0b427dc95297d
Sung Bae <singsang2@gmail.com>
2018-10-06 19:57:49 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -30,12 +30,21 @@ def initial_round
   display_card_total(card_total)
 end
 
-def hit?
-  # code hit? here
+def hit?(card_total)
+  prompt_user
+  input = get_user_input
+  if input == "h"
+    true
+  elsif input == "s"
+    false
+  else
+    invalid_command
+  end
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
+  prompt_user
 end
 
 #####################################################
