7382057911d47bb14cda2c10877d9c00f9f6260e
Sung Bae <singsang2@gmail.com>
2018-10-06 19:45:42 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -7,19 +7,19 @@ def deal_card
 end
 
 def display_card_total(number)
-  # code #display_card_total here
+  puts "Your cards add up to #{number}"
 end
 
 def prompt_user
-  puts ""
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  input = gets.strip
 end
 
 def end_game
-  # code #end_game here
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
